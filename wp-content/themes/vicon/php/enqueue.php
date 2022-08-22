51f0a06c87ab76f3dc1c727c1aad42a2f0de698c
Roloff <mads@betadesign.dk>
2022-08-22 13:30:05 +0200

    enqueue

--- wp-content/themes/vicon/php/enqueue.php
+++ wp-content/themes/vicon/php/enqueue.php
@@ -4,9 +4,9 @@ add_action("wp_enqueue_scripts", function () {
     wp_enqueue_script("jquery", "https://code.jquery.com/jquery-3.5.1.js");
     wp_enqueue_style("SwiperCSS", 'https://unpkg.com/swiper@7/swiper-bundle.min.css');
     wp_enqueue_script("SwiperJS", 'https://unpkg.com/swiper@7/swiper-bundle.min.js');
-	//wp_enqueue_script("HR", 'https://recruit.hr-on.com/frame-api/hr.js');
-	//wp_enqueue_script("HR2", 'https://recruit.hr-on.com/frame-api/customers/viconaps-unsolicited.js');
-	//wp_enqueue_script("HR3", 'https://recruit.hr-on.com/frame-api/customers/viconaps.js');
+	wp_enqueue_script("HR", 'https://recruit.hr-on.com/frame-api/hr.js');
+	wp_enqueue_script("HR2", 'https://recruit.hr-on.com/frame-api/customers/viconaps-unsolicited.js');
+	wp_enqueue_script("HR3", 'https://recruit.hr-on.com/frame-api/customers/viconaps.js');
     wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
     wp_enqueue_script("index", get_template_directory_uri() . "/js/index.js");
 
