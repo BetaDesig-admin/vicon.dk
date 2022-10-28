e48896ce49a4a4241204fe37706af93d7ddc6a16
Roloff <mads@betadesign.dk>
2022-10-28 14:37:41 +0200

    new swiper

--- wp-content/themes/vicon/php/enqueue.php
+++ wp-content/themes/vicon/php/enqueue.php
@@ -2,8 +2,9 @@
 add_action("wp_enqueue_scripts", function () {
     wp_enqueue_style("Font", 'https://use.typekit.net/zcs8ffj.css');
     wp_enqueue_script("jquery", "https://code.jquery.com/jquery-3.5.1.js");
-    wp_enqueue_style("SwiperCSS", 'https://unpkg.com/swiper@7/swiper-bundle.min.css');
-    wp_enqueue_script("SwiperJS", 'https://unpkg.com/swiper@7/swiper-bundle.min.js');
+    wp_enqueue_style("SwiperCSS", 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css');
+    wp_enqueue_script("SwiperJS", 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js');
+
 	//wp_enqueue_script("HR", 'https://recruit.hr-on.com/frame-api/hr.js');
 	//wp_enqueue_script("HR2", 'https://recruit.hr-on.com/frame-api/customers/viconaps-unsolicited.js');
 	//wp_enqueue_script("HR3", 'https://recruit.hr-on.com/frame-api/customers/viconaps.js');
@@ -16,8 +17,9 @@ add_action("wp_enqueue_scripts", function () {
 
 add_action('enqueue_block_editor_assets', function () {
     wp_enqueue_style("Font", 'https://use.typekit.net/zcs8ffj.css');
-    wp_enqueue_style("SwiperCSS", 'https://unpkg.com/swiper@7/swiper-bundle.min.css');
-    wp_enqueue_script("SwiperJS", 'https://unpkg.com/swiper@7/swiper-bundle.min.js');
+    wp_enqueue_style("SwiperCSS", 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css');
+    wp_enqueue_script("SwiperJS", 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js');
+
     wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
 }, 99);
 
